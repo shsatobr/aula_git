bc4c4dd11fbc597b7a52777dfa891ecd1d9bfe1d
Sergio Sato <shsatobr@outlook.com>
2022-08-17 12:50:37 +0000

    Inclusão do base.html e alteração no index.html

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("hello")
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def usuario():
+    usuario = [1, "Danilo de Souza", "Professor"]
+    alunos  = ["Andre Guedes", "Lucas Santos", "Alicia Duarte", "Raiane Caroline"]
+    return render_template("index.html", usuario = usuario, alunos=alunos)
 
 @app.route("/contato")
 def contato():
-    return "Sergio Hiroshi Sato"
+    nomeAula = "Aula Python para Web"
+    return render_template("index.html", nome = nomeAula, usuario = usuario)
